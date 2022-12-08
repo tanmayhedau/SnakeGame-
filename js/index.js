14436ddd7c448fb0da0e93ab4211800e8199ef4f
tanmayhedau <tanmayhedau619@gmail.com>
2022-12-08 17:41:50 +0530

    final push

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('food.mp3');
-const gameOverSound = new Audio('gameover.mp3');
-const moveSound = new Audio('move.mp3');
-const musicSound = new Audio('music.mp3');
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
 let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
